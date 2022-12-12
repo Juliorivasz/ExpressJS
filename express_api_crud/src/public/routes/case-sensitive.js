29c76cd80e4c2601f59ac30b839f65064c0cf452
Julio Rivas <julioandresrivas@gmail.com>
2022-12-12 20:01:06 -0300

    addded mods

--- express_api_crud/src/public/routes/case-sensitive.js
+++ express_api_crud/src/public/routes/case-sensitive.js
@@ -1,7 +1,8 @@
 // en vez de usar funciones podemos hacer uso de Router
 // es un metodo de express()
 
-require('ejs');
+require("ejs");
+const axios = require("axios");
 
 const { Router } = require("express");
 // es una funcion que debe ejecutarse
@@ -9,18 +10,27 @@ const { Router } = require("express");
 // se debe llamar la variable como router por convencion
 const router = Router();
 
-router.get("/Probando", (req, res) => {
-  res.send("probando case sensitive routing");
+router.get("/case", (req, res) => {
+  res.render("case");
 });
 
 // aplicando ejs vinculado al settings
-router.all("/ejs",(req,res)=>{
+router.all("/ejs", (req, res) => {
   // gracias a ejs podemos aplicar condigo de logica e introducirlo hacia el html
-  const title = "Aplicando logica desde el backend y renderizarlo en el html"
+  const title = "Aplicando logica desde el backend y renderizarlo en el html";
 
+  let isActive = false;
   // se le pasa como segundo parametro el objeto que queremos mostrar en el html
   // usando propiedad : valor, pero como tienen el mismo propiedad:valor se puede colocar solo el nombre
-  res.render("index", { title });
-})
+  res.render("index", { title, isActive });
+});
+
+router.get("/posts", async (req, res) => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  ).then((respon)=>console.log(respon))
+
+  res.render("posts", { posts: response.data });
+});
 
 module.exports = router;
